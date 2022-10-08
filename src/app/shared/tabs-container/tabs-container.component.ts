5ec31d4fa99905272f09f609897d7ecf04eccbaa
Douglas <douglasdothnc@gmail.com>
2022-10-08 16:15:05 +0100

    [DEV] Get Tabs for TabsContainer
    Use ContentChildren to get Tabs initialized, to successfully get the tabs we need to wait for the tabs to be initialized so we are changing implementation of TabsContainer from Oninit to AfterContentInit

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 // Class that manage a group of tabs and
 // allow the user the switch between them
@@ -7,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.css']
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements AfterContentInit {
+
+  // ContentChildren allow us to select elements from projected content
+  // We select TabComponents here
+  @ContentChildren(TabComponent) tabs = {}
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterContentInit(): void {
+    console.log(this.tabs)
   }
-
 }
