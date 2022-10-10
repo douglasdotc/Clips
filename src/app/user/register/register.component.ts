f104ca466d53d8fc10ff9634887ec4281de46b8d
Douglas <douglasdothnc@gmail.com>
2022-10-10 21:55:54 +0100

    [DEV] User registration and handle responses
    Use user authentication api in Firebase to register users and return appropriate messages if needed.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
 
 @Component({
   selector: 'app-register',
@@ -48,11 +49,40 @@ export class RegisterComponent {
   alertMsg = 'Please wait! Your account is being created.'
   alertColor = 'blue'
 
-  register() {
+  // Submission control
+  inSubmission = false
+
+  // auth service does not need to ne accessed in the template,
+  // so private to the component class
+  constructor(private auth: AngularFireAuth) {}
+
+  // async function return a Promise Object
+  async register() {
     this.showAlert = true
+    this.inSubmission = true
 
     // Reset message and color
     this.alertMsg = 'Please wait! Your account is being created.'
     this.alertColor = 'blue'
+
+    const { email, password } = this.registerForm.value
+
+    try {
+      const userCred = await this.auth.createUserWithEmailAndPassword(
+        email as string, password as string
+      )
+      console.log(userCred)
+
+    } catch(e) {
+      console.error(e) // Debug
+      var errMsg = (e as Error).message
+      this.alertMsg = errMsg
+      this.alertColor = 'red'
+      this.inSubmission = false
+      return
+    }
+
+    this.alertMsg = 'Success! Your account has been created.'
+    this.alertColor = 'green'
   }
 }
