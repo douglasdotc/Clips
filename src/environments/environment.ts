ca41fa617712cbeb6a36d609890eb486269a29ca
Douglas <douglasdothnc@gmail.com>
2022-10-10 21:28:29 +0100

    [DEV] Add firebase to the application

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDqRi0GtMmiJccdbCSv8uJViD1cHcYGmek", // key to connect to Firebase
+    authDomain: "clips-ec6fd.firebaseapp.com", // needed to login users to our app
+    projectId: "clips-ec6fd", // ID of the project
+    storageBucket: "clips-ec6fd.appspot.com", // to describe the location of the data stored
+    // messagingSenderId: "1066511690986", // feature that push notification to app instantly
+    appId: "1:1066511690986:web:87e88c8fde772877a4fb93" // app ID that connect to the project
+  }
 };
 
 /*
