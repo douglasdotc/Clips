12b9c218bbdc65bae5bdfd0a9e813d3c8899f53a
Douglas <douglasdothnc@gmail.com>
2022-10-12 21:17:29 +0100

    [DEV] Turn password auth method match() into a factory function

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,18 +1,20 @@
-import { ValidationErrors, AbstractControl } from "@angular/forms";
+import { ValidationErrors, AbstractControl, ValidatorFn } from "@angular/forms";
 
 export class RegisterValidators {
   // static methods can be called without instatiating the class
   // static methods cannot access variables in the class
   // static methods are good for creating utility methods but are limited at their scope
-  static match(group: AbstractControl): ValidationErrors | null {
-    const control = group.get('password')
-    const matchingControl = group.get('confirm_password')
+  static match(controlName: string, matchingConatrolName: string) : ValidatorFn {
+    return (group: AbstractControl) : ValidationErrors | null => {
+      const control = group.get(controlName)
+      const matchingControl = group.get(matchingConatrolName)
 
-    if (!control || !matchingControl) {
-      return { controlNotFound: false }
+      if (!control || !matchingControl) {
+        return { controlNotFound: false }
+      }
+
+      const error = control.value === matchingControl.value ? null : { noMatch: true }
+      return error
     }
-
-    const error = control.value === matchingControl.value ? null : { noMatch: true }
-    return error
   }
 }
