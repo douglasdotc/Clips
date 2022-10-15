ea63ea1494a557f0e93e6a98766ac97e5397bd33
Douglas <douglasdothnc@gmail.com>
2022-10-15 15:24:30 +0100

    [DEV] Add route for Home page
    Create a Home component to contain the contain of the home page.
    Add a route to app-routing.module.ts so that when the url path after the domain name is null, it will call the HomeComponent.
    Add a place holder <router-outlet> in the app.component.html template so that it will load the component corresponding to the path given

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // when the user call this path
+    component: HomeComponent // route to this component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
