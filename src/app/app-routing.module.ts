c50d727fcdd33eb9344927f89ff8fb93699fad9d
Douglas <douglasdothnc@gmail.com>
2022-10-15 15:41:53 +0100

    [DEV] Add route for About page
    Create About component
    Move content from about.html to the new component
    Add route for AboutComponent to app-routing
    Change href to routerLink in the nav template so that user can navigate to different pages without refresing the page (no need to download the page again)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '', // when the user call this path
     component: HomeComponent // route to this component
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 
