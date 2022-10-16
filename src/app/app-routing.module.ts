f8209ddce096bb49922e87a789332ab8c3b9c3c8
Douglas <douglasdothnc@gmail.com>
2022-10-16 14:16:43 +0100

    [DEV] Page not found
    Add a component that shows the page does not exist.
    Add wildcard routing for unknown paths in app-routing module.
    Modify the order of import in app module so that the wildcard works.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ClipComponent } from './clip/clip.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
@@ -17,6 +18,14 @@ const routes: Routes = [
     // :id is a placeholder for the id of the clip
     path: 'clip/:id',
     component: ClipComponent
+  },
+  {
+    // '**' wildcard to capture unknown paths
+    // Wildcard route should always at the end of this array.
+    // The Routing module check the routes in the order of this array.
+    // If this is at the top then the app will always route to 404 not found.
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { VideoModule } from './video/video.module';
 import { ClipComponent } from './clip/clip.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -20,16 +21,24 @@ import { ClipComponent } from './clip/clip.component';
     NavComponent,
     HomeComponent,
     AboutComponent,
-    ClipComponent
+    ClipComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     UserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
-    VideoModule // video routing module is imported inside Video module
+    // video routing module is imported inside Video module
+    // and will register corresponding routes upon import.
+    VideoModule,
+    // This module should be imported after any module that register child routes
+    // The wildcard path is located in app routing.
+    // if this is placed before any child routes,
+    // then users will always get routed to 404 not found
+    // because the child routes are not registered.
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
