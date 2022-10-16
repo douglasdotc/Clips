7b7c502bcf9c29d7a5565b594958d7dbe01798bc
Douglas <douglasdothnc@gmail.com>
2022-10-16 14:01:12 +0100

    [DEV] Add video sorting function
    Add sort function to manage which extract the selected element value as 'sort' and place it in the url route as a query parameter.
    Subscribe to the route and get the 'sort' parameter continuously from init.
    Call the sort function on change event and preserve the selected option by using the [selected] property binding.
    Initialize the manage page with the sort option = 1 in the nav bar.

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
+  // To store the sorting order
+  // 1: decending order (new upload first)
+  // 2: ascending order
+  videoOrder = '1'
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    // Subscribe to changes from the route parameters
+    // when the component is initialized. No need to unsubscribe
+    // the Observable since Angular will automatically unsubscribe from the
+    // Observable when the component is destroyed, which happens when ther user
+    // navigate to a different page.
+    this.route.queryParamMap.subscribe((params: Params) => {
+      this.videoOrder = params.get('sort') === '2' ? params.get('sort') : '1'
+    })
   }
 
+  sort(event: Event) {
+    // Get the value from event target in the selected element
+    const { value } = (event.target as HTMLSelectElement)
+
+    // Place the value in the url route as a query parameter:
+    // --> /manage?sort=${value}
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        sort: value
+      }
+    })
+  }
 }
