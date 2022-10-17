a00939cb63327d827d460b28e9e9711089f7e07b
Douglas <douglasdothnc@gmail.com>
2022-10-18 00:02:25 +0100

    [DEV] Video edit form
    Create a component that manage the video edit form, leveraging the Modal service developed earlier.
    Open and pass the active clip to the editClip modal.

---
+++ src/app/video/edit/edit.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import IClip from 'src/app/models/clip.model';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.css']
+})
+export class EditComponent implements OnInit, OnDestroy {
+  @Input() activeClip: IClip | null = null
+
+  constructor(
+    private modal: ModalService
+  ) { }
+
+  ngOnInit(): void {
+    this.modal.register('editClip')
+  }
+
+  ngOnDestroy(): void {
+    this.modal.unregister('editClip');
+  }
+}
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import IClip from 'src/app/models/clip.model';
 import { ClipService } from 'src/app/services/clip.service';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-manage',
@@ -14,11 +15,13 @@ export class ManageComponent implements OnInit {
   // 2: ascending order
   videoOrder = '1'
   clips: IClip[] = []
+  activeClip: IClip | null = null
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private clipService: ClipService
+    private clipService: ClipService,
+    private modal: ModalService
   ) { }
 
   ngOnInit(): void {
@@ -59,5 +62,12 @@ export class ManageComponent implements OnInit {
         sort: value
       }
     })
+  }
+
+  openModal($event: Event, clip: IClip) {
+    $event.preventDefault()
+
+    this.activeClip = clip
+    this.modal.toggleModal('editClip')
   }
 }
