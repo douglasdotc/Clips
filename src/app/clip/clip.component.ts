05bddf0190e468c71ca8b46abc34f688e10c7c9d
Douglas <douglasdothnc@gmail.com>
2022-10-22 22:05:04 +0100

    [DEV] Video player init
    Add videojs package so that we can play videos.
    Add a tag #videoPlayer to expose the video player to the component class file.
    Get videoPlayer in the component class as target.
    Create and initialize the videoPlayer as the target in the class.

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import videojs from 'video.js'
 
 @Component({
   selector: 'app-clip',
@@ -8,12 +9,29 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class ClipComponent implements OnInit {
   id = ''
+  // ViewChild() perform a query on the template and select specified elements:
+  // When we are selecting an element with ViewChild,
+  // the property will be stored as an ElementRef class.
+  // ViewChild() normally be called in ngAfterInit() because
+  // ngAfterInit() runs after the template is initialized.
+  // (e.g. after all the ngFor loops are done)
+  // Our videoPlayer is available from the beginning so we can access it
+  // from the beginning. By setting static = true, we run ViewChild() before ngOnInit()
+  // is called, by which point the data in the component is initialized.
+  @ViewChild('videoPlayer', { static: true }) target?: ElementRef
+
+  // Video Player instance:
+  player?: videojs.Player
 
   constructor(
     public route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    // Initialize the videoPlayer,
+    // videoPlayer needs direct access to the element (nativeElement):
+    this.player = videojs(this.target?.nativeElement)
+
     // Subscribe to params.id to receive the latest id from the user.
     this.route.params.subscribe((params: Params) => {
       this.id = params.id
